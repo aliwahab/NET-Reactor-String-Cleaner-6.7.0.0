0eda2c152a438e86bb532a5f07733b326134d47a
aliwahab <aliwahb@hotmail.com>
2026-01-05 22:53:30 +0200

    Refactor Context.cs to remove unused Assembly load

--- DNR 6.7 Strings/Core/Context.cs
+++ DNR 6.7 Strings/Core/Context.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Reflection;  // ADD THIS LINE
 using dnlib.DotNet;
 using dnlib.DotNet.Writer;
 
@@ -10,32 +9,23 @@ namespace DNR.Core
         public Context(CtxOptions ctxOptions)
         {
             Options = ctxOptions;
-            
-          
             Module = ModuleDefMD.Load(Options.FilePath);
             
-           
+            // REMOVE THIS LINE - causes BadImageFormatException
             // Asm = Assembly.UnsafeLoadFrom(Options.FilePath);
-            
-           
-            Asm = null;
             
             Console.WriteLine($"[INFO] Loaded: {Module.Name}");
         }
 
         public CtxOptions Options { get; }
         public ModuleDefMD Module { get; }
-        public Assembly Asm { get; set; }  // Will be null for .NET 6+ assemblies
 
         public void Save()
         {
             var writerOptions = new ModuleWriterOptions(Module)
             {
                 Logger = DummyLogger.NoThrowInstance,
-                MetadataOptions =
-                {
-                    Flags = MetadataFlags.PreserveAll & MetadataFlags.KeepOldMaxStack
-                }
+                MetadataOptions = { Flags = MetadataFlags.PreserveAll & MetadataFlags.KeepOldMaxStack }
             };
 
             Module.Write(Options.OutputPath, writerOptions);
